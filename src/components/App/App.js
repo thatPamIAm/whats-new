6be403f90ff550cccec669e352a46c37c32879d2
Pamela Lovett <pamela.s.lovett@gmail.com>
2019-10-19 09:30:35 -0700

    Create NewsContainer for articles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+
+import NewsContainer from '../NewsContainer/NewsContainer';
 import local from '../../data/local';
 import './App.css';
 
@@ -13,7 +15,7 @@ class App extends Component {
   render () {
     return (
       <div className="app">
-        YOUR CODE GOES HERE!
+        <NewsContainer articles={this.state.local}/>
       </div>
     );
   }
--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,21 @@
 import React from 'react';
+
+import NewsArticle from '../NewsArticle/NewsArticle';
 import './NewsContainer.css'
 
-// NEWSCONTAINER COMPONENT CODE GOES HERE
+
+const NewsContainer = (props) => {
+
+  const articles = props.articles.map(article => {
+    return <NewsArticle article={article} key={article.id} />
+  });
+
+
+  return (
+    <div>
+      {articles}
+    </div>
+  )
+}
 
 export default NewsContainer;
