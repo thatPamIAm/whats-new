e3e8cb1efe66b0e3afdf4472dae0447298a24267
Pamela Lovett <pamela.s.lovett@gmail.com>
2019-10-20 10:01:41 -0700

    Style article component
    Remove favicon
    
    Adjust width of app and news container
    
    Import data
    
    Adjust styling on articles
    
    Alphabetize rules

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './NewsArticle.css';
 
 const NewsArticle = (props) => {
-
+  
   const {img, headline, description, url} = props.article;
 
+
+
   return (
-    <div>
-      <img src={img} alt="Meaningful text" />
+    <div className="article-container">
+      <img style={{ backgroundImage : `url(${img})` }} />
       <h2>{headline}</h2>
       <p>{description}</p>
-      <a href={url}>Link To Article</a>
+      <a href={url}>Link To article</a>
     </div>
   )
 }
