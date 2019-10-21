7c0fca6d2983f3dd6660f4310e22af40eb7beeae
Pamela Lovett <pamela.s.lovett@gmail.com>
2019-10-21 15:02:24 -0600

    Update state in App add Menu functionality

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,21 @@
-// import React from 'react';
-// import './Menu.css'
+import React from 'react';
+import './Menu.css';
 
-// // MENU COMPONENT CODE GOES HERE
+const Menu = (props) => {
 
-// export default Menu;
+  const handleClick = (e) => {
+    props.updateNews(e.target.id)   
+  }
+
+  return (
+    <nav onClick={e => handleClick(e)}>
+      <li><button id="local">Local News</button></li>
+      <li><button id="technology">Technology</button></li>
+      <li><button id="entertainment">Entertainment</button></li>
+      <li><button id="science">Science</button></li>
+      <li><button id="health">Health</button></li>
+    </nav>
+  )
+}
+
+export default Menu;
