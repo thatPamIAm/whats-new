985e284c965bfe8e811ae44eccdaf027b45570c3
Pamela Lovett <pamela.s.lovett@gmail.com>
2019-10-22 10:26:47 -0600

    Add font awesome cdn and icons

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -8,17 +8,19 @@ class Menu extends Component {
   }
 
   handleClick(e) {
+    if (!e.target.id) return;
     this.props.updateNews(e.target.id)   
   }
 
   render() {
     return (
       <nav onClick={e => this.handleClick(e)}>
-        <li><button id="local">Local News</button></li>
-        <li><button id="technology">Technology</button></li>
-        <li><button id="entertainment">Entertainment</button></li>
-        <li><button id="science">Science</button></li>
-        <li><button id="health">Health</button></li>
+        <h2>What's <span>New?</span></h2>
+        <li><button id="local"><i class="fas fa-map-marker-alt"></i>Local News</button></li>
+        <li><button id="technology"><i class="fas fa-lightbulb"></i>Technology</button></li>
+        <li><button id="entertainment"><i class="fas fa-ticket-alt"></i>Entertainment</button></li>
+        <li><button id="science"><i class="fas fa-rocket"></i>Science</button></li>
+        <li><button id="health"><i class="fas fa-heartbeat"></i>Health</button></li>
       </nav>
     )
   }
