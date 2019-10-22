9e522befd5a925f813f403240e2cf94a2a11df7d
Pamela Lovett <pamela.s.lovett@gmail.com>
2019-10-21 21:03:30 -0600

    Update testing for App component

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -3,18 +3,37 @@ import { shallow } from 'enzyme';
 
 import App from './App';
 import local from '../../data/local';
+import entertainment from '../../data/entertainment';
+import health from '../../data/health';
+import science from '../../data/science';
+import technology from '../../data/technology';
 
 describe('App', () => {
-  it('should match the snapshot', () => {
-    const wrapper = shallow(<App />);
+  let wrapper;
 
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should have a default state of local news', () => {
-    const wrapper = shallow(<App />);
-
+  it('should have a default state', () => {
     expect(wrapper.state().local).toBe(local);
+    expect(wrapper.state().entertainment).toBe(entertainment);
+    expect(wrapper.state().health).toBe(health);
+    expect(wrapper.state().science).toBe(science);
+    expect(wrapper.state().technology).toBe(technology);
+    expect(wrapper.state().current).toBe(local);
+  });
+
+  it('should update the current news when updateNews is invoked', () => {
+    const mockNews = 'technology';
+
+    expect(wrapper.state().current).toBe(local);
+    wrapper.instance().updateNews(mockNews);
+    expect(wrapper.state().current).toBe(technology);
   });
 });
 
