6665770cee2214aaef25c6d47f8a020ce2f100db
Pamela Lovett <pamela.s.lovett@gmail.com>
2019-10-24 15:20:53 -0600

    Add fetch and remove imported data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,35 +1,46 @@
 import React, { Component } from 'react';
 
 import NewsContainer from '../NewsContainer/NewsContainer';
-import Menu from '../Menu/Menu'
-import SearchForm from '../SearchForm/SearchForm'
-import local from '../../data/local';
-import entertainment from '../../data/entertainment';
-import health from '../../data/health';
-import science from '../../data/science';
-import technology from '../../data/technology';
+import Menu from '../Menu/Menu';
+import SearchForm from '../SearchForm/SearchForm';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      local,
-      entertainment,
-      health,
-      science,
-      technology,
-      current: local,
+      local: [],
+      entertainment: [],
+      health: [],
+      science: [],
+      technology: [],
+      current: [],
       filteredCurrent: []
     }
   }
+
+  componentDidMount() {
+    fetch('https://whats-new-api.herokuapp.com/api/v1/news')
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({
+          local: data.local,
+          entertainment: data.entertainment,
+          health: data.health,
+          science: data.science,
+          technology: data.technology,
+          current: data.local
+        })
+      })
+      .catch(error => console.log(error))
+  }
+
 
   updateNews = (news) => {
     this.setState({
       current: this.state[news],
       filteredCurrent: []
-    });
-
+    })
   }
 
   filter = (query) => {
@@ -41,8 +52,6 @@ class App extends Component {
       filteredCurrent: filtered
     }); 
   }
-
-
 
   render () {
     const length = this.state.filteredCurrent.length;
